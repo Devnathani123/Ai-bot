5667eaedc24b548cf3250c52549dc5fcc1fd9423
Devnathani123 <108208655+Devnathani123@users.noreply.github.com>
2024-10-07 15:29:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ HTML_TEMPLATE = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>AI Chatbot</title>
+    <title>Chatie ai </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
